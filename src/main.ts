ade6eb2c3b5ce8785e284170f5c05716d54a971a
YEBISUYA Sugoroku <yebisuya@gmail.com>
2024-09-26 23:58:10 +0900

    fix: HTTPS以外のときはServiceWorkerの登録を行わないように修正

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ declare const contentBox: HTMLDivElement;
 window.addEventListener('load', () => {
   prepareEditor(contentBox);
   contents(contentBox);
-  if ('serviceWorker' in navigator) {
+  if ('serviceWorker' in navigator && location.protocol === 'https:') {
     navigator.serviceWorker.register('/memo/sw.js');
   }
 });
