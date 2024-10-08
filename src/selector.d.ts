1144afe81190f96c0f8d85a4990c99628f2efc47
YEBISUYA Sugoroku <yebisuya@gmail.com>
2024-10-08 22:04:32 +0900

    refactor: HTML以外の要素にも対応可能にしておく

--- src/selector.d.ts
+++ src/selector.d.ts
@@ -1,34 +1,60 @@
-type ResultHTMLSelector<K extends string> = K extends '*'
-  ? HTMLElement
-  : K extends keyof HTMLElementTagNameMap
-  ? HTMLElementTagNameMap[K]
+type AllElementTagNameMap = HTMLElementTagNameMap &
+  Omit<SVGElementTagNameMap, keyof HTMLElementTagNameMap> &
+  Omit<
+    MathMLElementTagNameMap,
+    keyof HTMLElementTagNameMap | keyof SVGElementTagNameMap
+  >;
+
+type ResultQuerySelector<
+  K extends string,
+  Default extends Element = Element,
+  TagNameMap extends object = AllElementTagNameMap
+> = K extends '*'
+  ? Default
+  : K extends keyof TagNameMap
+  ? TagNameMap[K]
   : K extends `${infer FIRST},${infer REST}`
-  ? ResultHTMLSelector<FIRST> | ResultHTMLSelector<REST>
-  : K extends `${string}${' ' | '>' | '+' | '~'}${infer KK}`
-  ? ResultHTMLSelector<KK>
+  ? ResultQuerySelector<FIRST> | ResultQuerySelector<REST>
+  : K extends `${string}${' ' | '>' | '+' | '~' | '|'}${infer KK}`
+  ? ResultQuerySelector<KK>
   : K extends `${infer KK}${'.' | '[' | '#' | ':'}${string}`
   ? KK extends `${string}${'.' | '[' | '#' | ':'}${string}`
     ? never
-    : KK extends keyof HTMLElementTagNameMap
-    ? HTMLElementTagNameMap[KK]
-    : HTMLElement
+    : KK extends keyof TagNameMap
+    ? TagNameMap[KK]
+    : Default
   : never;
+type ResultHTMLQuerySelector<K extends string> = ResultQuerySelector<
+  K,
+  HTMLElement,
+  HTMLElementTagNameMap
+>;
 
 interface ParentNode {
-  querySelector<K extends string>(selectors: K): ResultHTMLSelector<K> | null;
+  querySelector<K extends string>(selectors: K): ResultQuerySelector<K> | null;
   querySelectorAll<K extends string>(
     selectors: K
-  ): NodeListOf<ResultHTMLSelector<K>>;
+  ): NodeListOf<ResultQuerySelector<K>>;
+}
+
+interface Element {
+  closest<K extends string>(selector: K): ResultQuerySelector<K> | null;
 }
 
 interface HTMLElement {
-  closest<K extends string>(selector: K): ResultHTMLSelector<K> | null;
+  querySelector<K extends string>(
+    selectors: K
+  ): ResultHTMLQuerySelector<K> | null;
+  querySelectorAll<K extends string>(
+    selectors: K
+  ): NodeListOf<ResultHTMLQuerySelector<K>>;
+  closest<K extends string>(selector: K): ResultHTMLQuerySelector<K> | null;
 }
 
 type LocalNameFor<
-  T extends HTMLElement,
-  K extends keyof HTMLElementTagNameMap = keyof HTMLElementTagNameMap
-> = K extends K ? (HTMLElementTagNameMap[K] extends T ? K : never) : never;
+  T extends Element,
+  K extends keyof AllElementTagNameMap = keyof AllElementTagNameMap
+> = K extends K ? (AllElementTagNameMap[K] extends T ? K : never) : never;
 
 interface HTMLTableCellElement {
   localName: LocalNameFor<HTMLTableCellElement>;
