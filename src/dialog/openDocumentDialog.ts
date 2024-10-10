9a8bed6652c2eb78ba740b30afd84be5204e6f59
YEBISUYA Sugoroku <yebisuya@gmail.com>
2024-10-10 21:21:29 +0900

    refactor: フォーカス移動で選択項目を変更できるように修正

--- src/dialog/openDocumentDialog.ts
+++ src/dialog/openDocumentDialog.ts
@@ -1,15 +1,14 @@
 function openDocumentDialog() {
-  let selected: string | undefined;
   const dlg = dialog({classList: 'open-document'})/*html*/ `
-      <button value="cancel" title="閉じる" tabIndex="-1"></button>
-      <div class="list" tabIndex="1" autofocus>
-        <div class="list-item-new">
-          <div class="list-item-title"><button value="new" title="新しいメモ"></button></div>
-          <div class="list-item-last-modified">最終更新日時</div>
-          <div class="list-item-buttons"></div>
-        </div>
+    <div class="list">
+      <div class="list-item-new">
+        <div class="list-item-title"><button value="new" title="新しいメモ"></button></div>
+        <div class="list-item-last-modified">最終更新日時</div>
+        <div class="list-item-buttons"></div>
       </div>
-    `;
+    </div>
+    <button value="cancel" title="閉じる" tabIndex="-1"></button>
+  `;
   const list = dlg.querySelector('div.list')!;
   const currentDocumentId = documentId;
   (async () => {
@@ -17,9 +16,9 @@ function openDocumentDialog() {
       if (id === currentDocumentId) {
         continue;
       }
-      list.append(element('div', {
+      const item = element('div', {
         classList: 'list-item',
-        data: {id},
+        properties: {tabIndex: 0},
       })/* html */ `
           <div class="list-item-title" title="${title}">${title}</div>
           <div class="list-item-last-modified">${formatDate(
@@ -27,38 +26,84 @@ function openDocumentDialog() {
             lastModified
           )}</div>
           <div class="list-item-buttons">
-            <button type="button" name="open" title="開く" tabIndex="-1"></button>
-            <button type="button" name="another-tab" title="別タブで開く" tabIndex="-1"></button>
-            <button type="button" name="delete" title="削除" tabIndex="-1"></button>
+            <button type="button" name="open" title="開く"></button>
+            <button type="button" name="another-tab" title="別タブで開く"></button>
+            <button type="button" name="delete" title="削除"></button>
           </div>
-        `);
+        `;
+      item.addEventListener('focus', () => {
+        selectItem(item);
+      });
+      item.addEventListener('blur', ev => {
+        if ((ev.relatedTarget as HTMLElement)?.closest('.list-item') !== item) {
+          item.removeAttribute('data-selected');
+          item.blur();
+        }
+      });
+      item.addEventListener('pointerenter', () => {
+        selectItem(item);
+      });
+      item.addEventListener('pointerleave', () => {
+        item.removeAttribute('data-selected');
+        item.blur();
+      });
+      item.addEventListener('keydown', ev => {
+        if (
+          ev.target === item &&
+          !ev.shiftKey &&
+          !ev.ctrlKey &&
+          !ev.altKey &&
+          ev.key === 'Enter'
+        ) {
+          item.click();
+        }
+      });
+      item.addEventListener('click', ev => {
+        switch ((ev.target as HTMLElement).closest('button')?.name) {
+          case 'another-tab':
+            (async () => {
+              const hash = await loadDocument(id);
+              window.open(`#${hash}`, '_blank');
+            })();
+            break;
+          case 'delete':
+            (async () => {
+              const answer = await confirmDialog(
+                `${title}を削除します。\nよろしいですか?`
+              );
+              if (!answer) {
+                return;
+              }
+              await deleteDocument(id);
+              item.remove();
+            })();
+            break;
+          default:
+            (async () => {
+              const hash = await loadDocument(id);
+              dlg.close();
+              documentId = id;
+              location.hash = `#${hash}`;
+            })();
+            break;
+        }
+      });
+      list.append(item);
     }
   })();
-  const selectItem = (item?: Element) => {
-    list.focus();
+  const selectItem = (item: HTMLElement) => {
+    item.focus();
     for (const other of list.querySelectorAll('[data-selected]')) {
       other.removeAttribute('data-selected');
     }
-    for (const button of list.querySelectorAll(
-      '.list-item button:not([tabindex="-1"])'
-    )) {
-      button.tabIndex = -1;
-    }
-    if (!item) {
-      return;
-    }
     item.setAttribute('data-selected', 'true');
-    for (const button of item.querySelectorAll('button[tabindex]')) {
-      button.tabIndex = 0;
-    }
-    selected = item.getAttribute('data-id') ?? undefined;
   };
   const selectNextItem = (direction: 'forward' | 'backword') => {
     const selected = list.querySelector(`.list-item[data-selected]`);
     const item = selected
-      ? selected?.[
+      ? (selected?.[
           `${direction === 'forward' ? 'next' : 'previous'}ElementSibling`
-        ]
+        ] as HTMLElement)
       : list.querySelector('.list-item');
     if (!item?.classList.contains('list-item')) {
       return false;
@@ -66,85 +111,25 @@ function openDocumentDialog() {
     selectItem(item);
     return true;
   };
-  list.addEventListener('keydown', ev => {
-    switch (`${ev.ctrlKey ? 'ctrl+' : ''}${ev.altKey ? 'alt+' : ''}${ev.key}`) {
+  dlg.addEventListener('keydown', ev => {
+    switch (
+      `${ev.key.length > 1 && ev.shiftKey ? 'shift+' : ''}${
+        ev.ctrlKey ? 'ctrl+' : ''
+      }${ev.altKey ? 'alt+' : ''}${ev.key}`
+    ) {
       case 'ArrowUp':
         selectNextItem('backword') && ev.preventDefault();
         return;
       case 'ArrowDown':
         selectNextItem('forward') && ev.preventDefault();
         return;
-      case 'Enter':
-        {
-          const target =
-            (ev.target as HTMLElement).closest('button') ??
-            list.querySelector(`.list-item[data-id="${selected}"]`);
-          if (!target) {
-            break;
-          }
-          target.click();
-          ev.preventDefault();
-          ev.stopPropagation();
-        }
-        return;
     }
   });
-  dlg.addEventListener('close', ev => {
+  dlg.addEventListener('close', () => {
     if (dlg.returnValue === 'new') {
       location.replace(location.pathname);
       return;
     }
-  });
-  dlg.addEventListener('click', ev => {
-    const target = ev.target as HTMLElement;
-    const button = target.closest('button');
-    if (button?.type === 'submit') {
-      return;
-    }
-    const item = target.closest('.list-item');
-    if (!item) {
-      selectItem();
-      return;
-    }
-    const id = item.getAttribute('data-id')!;
-    switch (button?.name) {
-      case 'another-tab':
-        (async () => {
-          const hash = await loadDocument(id);
-          window.open(`#${hash}`, '_blank');
-        })();
-        ev.preventDefault();
-        ev.stopPropagation();
-        break;
-      case 'delete':
-        (async () => {
-          const answer = await confirmDialog(
-            `${
-              item.querySelector('.list-item-title')?.title ?? ''
-            }を削除します。\nよろしいですか?`
-          );
-          if (!answer) {
-            return;
-          }
-          await deleteDocument(id);
-          item.remove();
-        })();
-        ev.preventDefault();
-        ev.stopPropagation();
-        break;
-      default:
-        (async () => {
-          const hash = await loadDocument(id);
-          if (!hash) {
-            return;
-          }
-          dlg.close();
-          documentId = selected;
-          location.hash = `#${hash}`;
-        })();
-        break;
-    }
-    return;
   });
   document.body.append(dlg);
   dlg.showModal();
