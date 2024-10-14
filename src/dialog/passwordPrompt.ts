d1416d01af6365b29e94f9fda35c798ce5c724cc
YEBISUYA Sugoroku <yebisuya@gmail.com>
2024-10-14 10:30:00 +0900

    docs: JsDocに引数の説明を追加

--- src/dialog/passwordPrompt.ts
+++ src/dialog/passwordPrompt.ts
@@ -7,9 +7,12 @@
  * - 5文字以上入力されないと設定ボタンはグレーアウトしてダイアログを閉じることができない
  * - ESCキーでもダイアログは閉じない。
  * - パスワードが5文字以上入力されていればEnterキーでもダイアログは閉じる
+ * @param options パスワード入力時のオプション。省略可
+ * @param options.minLength パスワードの必須文字数。省略時は5
  * @returns {Promise<string>} 入力されたパスワード
  */
-async function passwordPrompt({minLength = 5}: {minLength?: number} = {}) {
+async function passwordPrompt(options: {minLength?: number} = {}) {
+  const {minLength = 5} = options;
   const randomPassword = Math.random().toString(36).slice(2);
   const dlg = dialog({classList: 'password-prompt'})/* html */ `
     <label>パスワード:</label>
