0acfb2d4b4ee9ceb3df488304fdcf70e22131248
YEBISUYA Sugoroku <yebisuya@gmail.com>
2024-10-19 00:03:19 +0900

    feat: パスワードダイアログで確認用の入力を排除

--- src/dialog/passwordPrompt.ts
+++ src/dialog/passwordPrompt.ts
@@ -13,57 +13,38 @@
  */
 async function passwordPrompt(options: {minLength?: number} = {}) {
   const {minLength = 5} = options;
+  // 自動生成したパスワード
   const randomPassword = Math.random().toString(36).slice(2);
   const dlg = dialog({
     classList: 'password-prompt',
-    title: 'パスワードの設定',
+    title: ' ',
   })/* html */ `
-    <label>パスワード:</label>
-    <input type="text"
-           required
-           minLength="${minLength}"
-           value="${randomPassword}"/>
-    <summary>
-      内容をURLに積む際の暗号化に使用されるパスワードです<br>
-      同じパスワードを使うことで別のブラウザでも同じURLを<br>
-      表示および編集できます。
-    </summary>
-    <label>パスワード(確認):</label>
-    <input type="password"
-           required
-           minLength="${minLength}"
-           value="${randomPassword}"/>
-    <summary>
-      パスワードに打ち間違いがないか確認します。<br>
-      上のパスワードと同じものを入力してください。
-    </summary>
-    <button>設定</button>
+    <label></label>
+    <div>
+      <input required
+            minLength="${minLength}"
+            value="${randomPassword}"/>
+      <label><input type="checkbox" checked/></label>
+    </div>
+    <summary></summary>
+    <button></button>
   `;
-  const [input, input2] = dlg.querySelectorAll('input');
-  const customValidation = (valid: boolean) => {
-    input2.setCustomValidity(valid ? '' : '確認用のパスワードが一致しません。');
-    input2.reportValidity();
-  };
+  const input = dlg.querySelector('input:not([type])')!;
+  const checkbox = dlg.querySelector('input[type=checkbox]')!;
   input.addEventListener(
     'input',
     () => {
-      // 入力があればパスワードフィールドに変えて確認用フィールドをクリアする
+      // 初回だけ入力があればパスワードフィールドに変える
       input.type = 'password';
-      input2.value = '';
+      checkbox.checked = false;
     },
     {once: true}
   );
-  input2.addEventListener('input', () => {
-    // 入力があったら一時的に確認結果を解除
-    if (input2.validity.customError) {
-      customValidation(true);
-    }
+  checkbox.addEventListener('change', () => {
+    // 目のアイコンをクリックすることでパスワード入力フィールドと通常のテキストフィールドを切り替える
+    input.type = checkbox.checked ? 'text' : 'password';
   });
-  input2.addEventListener('change', () => {
-    // 値が変更されたら再度確認
-    customValidation(input.value === input2.value);
-  });
-  // ダイアログを開いたときに選択状態にする
+  // ダイアログを開いたときに自動生成したパスワードを選択状態にする
   input.select();
   document.body.append(dlg);
   dlg.showModal();
