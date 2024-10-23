6450fdb2a13776394054670e496e4c3dd6e308c8
YEBISUYA Sugoroku <yebisuya@gmail.com>
2024-10-24 01:12:26 +0900

    refactor: workerに他からでもアクセスできるように

--- src/main.ts
+++ src/main.ts
@@ -1,20 +1,20 @@
 declare const menu: HTMLDivElement;
 declare const contentBox: HTMLDivElement;
+let worker: ServiceWorker | null | undefined;
 
 window.addEventListener('load', () => {
   prepareEditor(contentBox);
   contents(contentBox);
   if ('serviceWorker' in navigator && location.protocol === 'https:') {
     (async () => {
-      const {active: worker} = await navigator.serviceWorker.register(
-        './sw.js'
-      );
+      worker = (await navigator.serviceWorker.register('./sw.js')).active;
       if (worker) {
+        const w = worker;
         window.addEventListener(
           'keydown',
           ev => {
             if (!ev.ctrlKey && !ev.altKey && ev.key === 'F5') {
-              worker.postMessage({type: 'cache-clear'});
+              w.postMessage({type: 'cache-clear'});
             }
           },
           true
