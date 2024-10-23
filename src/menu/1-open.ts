c11be6fd30c7e6b61d21e64a4b28e7b0ce3a82c7
YEBISUYA Sugoroku <yebisuya@gmail.com>
2024-10-24 01:16:20 +0900

    fix: デザイン調整用の補助線を削除

--- src/menu/1-open.ts
+++ src/menu/1-open.ts
@@ -4,7 +4,6 @@ window.addEventListener('DOMContentLoaded', () => {
   })/* html */ `
     <svg xmlns="http://www.w3.org/2000/svg" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round"
     stroke-linejoin="round" viewBox="0 0 24 24">
-    <circle cx="12" cy="12" r="13" stroke-width="0.1"/>
     <path d="M4 20h13l6-6h-13z"/>
     <path d="M7 16v-10a2 2 0 0 1 2-2h10a2 2 0 0 1 2 2v7 m-11-5h8m0 3h-8"/>
     <path d="M4 20v-13h3"/>
