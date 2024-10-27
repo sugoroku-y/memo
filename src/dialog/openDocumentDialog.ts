8a617479d5b8fba39a1bc6b13b605c6739904979
YEBISUYA Sugoroku <yebisuya@gmail.com>
2024-10-28 00:50:27 +0900

    refactor: 開くアイコンの微調整

--- src/dialog/openDocumentDialog.ts
+++ src/dialog/openDocumentDialog.ts
@@ -232,12 +232,12 @@ function openDocumentDialog(currentDocumentId?: string) {
           <div class="list-item-buttons">
             <button type="button" name="open" title="開く" autofocus>
               <svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-                <path d="M2 18v-14a2 2 0 0 1 2-2h16a2 2 0 0 1 2 2v16a2 2 0 0 1-2 2h-14m-2-2l14-14v4m0-4h-4"/>
+                <path d="M6 22a2 2 0 0 1-2-2v-10m0-4v-2a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v16a2 2 0 0 1-2 2h-12M2 8h13m-3 3l3-3-3-3"/>
               </svg>
             </button>
             <button type="button" name="another-tab" title="別タブで開く">
               <svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
-                <path d="M2 18v-14a2 2 0 0 1 2-2h16a2 2 0 0 1 2 2v11m0 5a2 2 0 0 1-2 2m-5 0h-9m-2-2l14-14v4m0-4h-4M17.5 17.5m-4.5 0h10m-5.5-4.5v10"/>
+                <path d="M6 22a2 2 0 0 1-2-2v-10m0-4v-2a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v10m0 6a2 2 0 0 1-2 2m-6 0h-6M2 8h13m-3 3l3-3-3-3M15 22v-10m-5 5h10"/>
               </svg>
             </button>
             <button type="button" name="delete" title="削除">
